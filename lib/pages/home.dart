38b24e13926f1b8e6ce393e3304d99a825e29537
Áquila Oliveira <oaquila14@gmail.com>
2023-08-18 22:08:11 -0300

    feat: first part page question

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart'; // Importe o pacote do carrossel
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:quiz_app/pages/question.dart'; // Importe o pacote do carrossel
 
 class Home extends StatelessWidget {
   @override
@@ -41,71 +42,97 @@ class Home extends StatelessWidget {
                 enableInfiniteScroll: false),
             items: [
               // Itens do carrossel
-              Container(
-                width: 250, // Defina a largura desejada para cada item
-                decoration: BoxDecoration(
-                  color: Color(0xFFFF6F00),
-                  borderRadius:
-                      BorderRadius.circular(20), // Define o raio da borda
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                        padding: EdgeInsets.only(
-                            left: 20.0,
-                            top: 16.0), // Adicione margem a todos os lados
-                        child: Text('Filmes',
-                            style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xFFFFFFFF)))),
-                    SizedBox(height: 10),
-                    Padding(
-                        padding: EdgeInsets.only(
-                            left: 20.0,
-                            top: 8.0), // Adicione margem a todos os lados
+              // Item 1
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            Question()),
+                  );
+                },
+                child: Container(
+                  width: 250,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFF6F00),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 20.0, top: 16.0),
                         child: Text(
-                            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Mais texto haha e bem mais',
-                            style: TextStyle(
-                                fontSize: 24, color: Color(0xFFFFFFFF)))),
-                  ],
+                          'Filmes',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFFFFFFF),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Padding(
+                        padding: EdgeInsets.only(left: 20.0, top: 8.0),
+                        child: Text(
+                          'Lorem Ipsum is simply dummy text...',
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Color(0xFFFFFFFF),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Container(
-                width: 250, // Defina a largura desejada para cada item
-                decoration: BoxDecoration(
-                  color: Color(0xFFFF6F00),
-                  borderRadius:
-                      BorderRadius.circular(20), // Define o raio da borda
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                        padding: EdgeInsets.only(
-                            left: 20.0,
-                            top: 16.0), // Adicione margem a todos os lados
-                        child: Text('Esportes',
-                            style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xFFFFFFFF)))),
-                    SizedBox(height: 10),
-                    Padding(
-                        padding: EdgeInsets.only(
-                            left: 20.0,
-                            top: 8.0), // Adicione margem a todos os lados
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            Question()), 
+                  );
+                },
+                child: Container(
+                  width: 250,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFF6F00),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 20.0, top: 16.0),
                         child: Text(
-                            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Mais texto haha e bem mais',
-                            style: TextStyle(
-                                fontSize: 24, color: Color(0xFFFFFFFF)))),
-                  ],
+                          'Esportes',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFFFFFFF),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Padding(
+                        padding: EdgeInsets.only(left: 20.0, top: 8.0),
+                        child: Text(
+                          'Lorem Ipsum is simply dummy text...',
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Color(0xFFFFFFFF),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              // Adicione mais itens do carrossel conforme necessário
             ],
           ),
         ],
--- lib/pages/question.dart
+++ lib/pages/question.dart
@@ -1,12 +1,51 @@
-
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Tela Secundária')),
-      body: Center(child: Text('Conteúdo da Tela Secundária')),
+      appBar: AppBar(
+        title: Text(''),
+        backgroundColor: Color(0xFFFF6F00),
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: 1000, 
+            height: 220,
+            decoration: BoxDecoration(
+              color: Color(0xFFFF6F00), 
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                    padding: EdgeInsets.only(
+                        left: 20.0,
+                        top: 16.0), // Adicione margem a todos os lados
+                    child: Text('Esportes',
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFFFFFFF)))),
+                SizedBox(height: 10),
+                Padding(
+                    padding: EdgeInsets.only(
+                        left: 20.0,
+                        top: 8.0), // Adicione margem a todos os lados
+                    child: Text(
+                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Mais texto haha e bem mais',
+                        style:
+                            TextStyle(fontSize: 24, color: Color(0xFFFFFFFF)))),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
